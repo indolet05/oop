bb9dc7be396f404fa7d15c1f33f2a08f08fa2ea9
indolet05 <79007259+indolet05@users.noreply.github.com>
2024-10-08 21:51:09 +0500

    Add files via upload

---
+++ Pet.py
@@ -0,0 +1,32 @@
+Class Pet:
+	def _init_(name , age):
+        self.name = name
+        self.age = age
+        
+        def get_name(self):
+            return self.name
+            
+        def get_age(self):
+            return self.age
+            
+        def set_name():
+            
+            
+        def set_age():
+        
+        
+        
+        
+Class Dog(Pet):
+    def _init_(self, name, age, bark())
+        super _init_(
+        
+        
+        
+Class Cat(Pet):
+    def _init_(self, name, age, meow())
+    
+    
+    
+Class Bird(Pet):
+        def _init_(self, name, age, chirp())
